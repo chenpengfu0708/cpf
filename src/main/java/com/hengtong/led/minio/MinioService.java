5124c4224ce6b733594504eb31d567fcadaf422b
chenpengfu <1074454337@qq.com>
2020-01-14 17:16:13 +0800

    优化

--- src/main/java/com/hengtong/led/minio/MinioService.java
+++ src/main/java/com/hengtong/led/minio/MinioService.java
@@ -19,17 +19,17 @@ public class MinioService {
                     minioConfig.getSecretKey());
 
             // 检查存储桶是否已经存在
-            boolean isExist = minioClient.bucketExists("test");
+            boolean isExist = minioClient.bucketExists(minioConfig.getBucketName());
             if(isExist) {
                 System.out.println("Bucket already exists.");
             } else {
                 // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
-                minioClient.makeBucket("test");
+                minioClient.makeBucket(minioConfig.getBucketName());
             }
             // 使用putObject上传一个文件到存储桶中。
-            minioClient.putObject("test",file.getOriginalFilename(), file.getInputStream(), file.getContentType());
+            minioClient.putObject(minioConfig.getBucketName(),file.getOriginalFilename(), file.getInputStream(), file.getContentType());
             System.out.println("successfully...");
-            return minioClient.getObjectUrl("test", file.getOriginalFilename());
+            return minioClient.getObjectUrl(minioConfig.getBucketName(), file.getOriginalFilename());
         } catch(Exception e) {
             System.out.println("Error occurred: " + e);
         }
