cb95d2a36292b035fd8df3cb85afe10c8c0a3a05
chenpengfu <1074454337@qq.com>
2020-04-07 17:23:55 +0800

    update

--- src/main/java/com/hengtong/led/controller/FileUploadController.java
+++ src/main/java/com/hengtong/led/controller/FileUploadController.java
@@ -1,9 +1,11 @@
 package com.hengtong.led.controller;
 
+import com.hengtong.led.dto.HandlerDto;
 import com.hengtong.led.fileUpload.dto.FileUploadResponse;
 import com.hengtong.led.fileUpload.service.FileUploadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,9 @@ public class FileUploadController {
 
 
     @RequestMapping(value = "/illegalBuilding/upload", method = RequestMethod.POST)
-    public FileUploadResponse illegalBuildingUpload(MultipartFile file) {
-        log.info("文件上传---------");
+    public FileUploadResponse illegalBuildingUpload(MultipartFile file, HandlerDto request) {
+        log.info("文件上传---------，body:{}", request);
+        //2020.4.7 新增一个body，用于记录MultipartFile 与 body公用，去掉@RequestBody注解即可共用
         return fileUploadService.upload(file);
     }
 }
